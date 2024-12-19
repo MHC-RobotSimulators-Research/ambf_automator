43a9bd7475a71d1d3e32b0c6fa9ef051e4cd575e
nataliechalfant <natchalfant@gmail.com>
2024-12-18 23:29:50 -0500

    refined simulation parameters ranges

---
+++ scripts/config.py
@@ -0,0 +1,24 @@
+base_env = '/home/nataliechalfant/ambf_automator/launch.yaml'
+target_body = '/home/nataliechalfant/ambf_automator/ruth_1000/default_soft.yaml'
+
+data_location = '/home/nataliechalfant/ambf_automator/ruth_1000/'
+data_adf_location = data_location + 'data_adf/'
+data_ipc_location = data_location + 'data_ipc/'
+data_fpc_location = data_location + 'data_fpc/'
+
+test_name = 'ruth_1000_soft'
+
+kLST_min = 0
+kLST_max = 0.9
+kLST_range = kLST_max - kLST_min
+kLST_steps = 10
+
+kDF_min = 0
+kDF_max = 1
+kDF_range = kDF_max - kDF_min
+kDF_steps = 10
+
+kMT_min = 0.0001
+kMT_max = 0.01
+kMT_range = kMT_max - kMT_min
+kMT_steps = 10
